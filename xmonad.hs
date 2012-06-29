2b95d0a66897d0ca7fb5a228f46c3a56eb375715
Sebastian Book <sebbebook@gmail.com>
2012-06-29 14:53:25 +0200

    added xdefaults, xmobarrc and xmonad.hs files

---
+++ xmonad.hs
@@ -0,0 +1,72 @@
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import System.IO
+import System.Exit
+ 
+import qualified XMonad.StackSet as W
+import qualified Data.Map        as M
+
+myTerminal      = "urxvt"
+myBorderWidth   = 1
+myModMask       = mod1Mask
+myNormalBorderColor  = "#000000"
+myFocusedBorderColor = "#333333"
+myWorkspaces    = ["1","2","3","4","5","6","7","8","9"]
+ 
+
+myLayout = tiled ||| Mirror tiled ||| Full
+  where
+     tiled   = Tall nmaster delta ratio
+     nmaster = 1
+     ratio   = 1/2
+     delta   = 3/100
+
+myLogHook = return ()
+myStartupHook = return ()
+
+myMouseBindings (XConfig {XMonad.modMask = modMask}) = M.fromList $
+    [ ((modMask, button1), (\w -> focus w >> mouseMoveWindow w))
+    , ((modMask, button2), (\w -> focus w >> mouseResizeWindow w))
+    ]
+
+myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
+    [ ((modm .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf)
+    , ((modm,               xK_p     ), spawn "exe=`dmenu_path | dmenu` && eval \"exec $exe\"")
+    , ((modm .|. shiftMask, xK_p     ), spawn "gmrun")
+    , ((modm .|. shiftMask, xK_c     ), kill)
+    , ((modm,               xK_space ), sendMessage NextLayout)
+    , ((modm .|. shiftMask, xK_space ), setLayout $ XMonad.layoutHook conf)
+    , ((modm,               xK_Tab   ), windows W.focusDown)
+    , ((modm,               xK_j     ), windows W.focusDown)
+    , ((modm,               xK_k     ), windows W.focusUp  )
+    , ((modm,               xK_h     ), sendMessage Shrink)
+    , ((modm,               xK_l     ), sendMessage Expand)
+    , ((modm .|. shiftMask, xK_q     ), io (exitWith ExitSuccess))
+    , ((modm              , xK_q     ), restart "xmonad" True)
+    ]
+    ++
+
+    [((m .|. modm, k), windows $ f i)
+        | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
+        , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
+ 
+
+main = do
+    xmproc <- spawnPipe "xmobar"
+    xmonad $ defaultConfig
+        { manageHook = manageDocks <+> manageHook defaultConfig
+        , layoutHook = avoidStruts $ layoutHook defaultConfig
+        , modMask = myModMask
+        , terminal = myTerminal
+        , focusFollowsMouse = True
+        , borderWidth = myBorderWidth
+        , workspaces = myWorkspaces
+        , normalBorderColor = myNormalBorderColor
+        , focusedBorderColor = myFocusedBorderColor
+        , keys = myKeys
+        , mouseBindings = myMouseBindings
+        , logHook = myLogHook
+        , startupHook = myStartupHook
+        }
