0bc1873e4bb89069839da471b17691275b6f89bf
Sebastian Bengtegård <sebbebook@gmail.com>
2016-01-20 21:52:23 +0100

    added new scripts, and updated mail config

---
+++ scripts/directory_fix.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# List of characters
+# ==================
+# 
+# ô == Ö
+# î == ö
+# Ü == å
+# è == Å
+# Ñ == ä
+# TODO, ? == Ä
+
+# If no argument was provided set directory to "."
+SEARCHDIR=`echo ${1:-.}`
+
+# If the directory to be searched also contains incorrect characters
+if [[ $SEARCHDIR == *ô* ]] || [[ $SEARCHDIR == *î* ]] || [[ $SEARCHDIR == *Ü* ]] || [[ $SEARCHDIR == *è* ]] || [[ $SEARCHDIR == *Ñ* ]]; then
+    # Temporary variable of the current directory
+    CUR_SEARCHDIR=$SEARCHDIR
+    # Search and replace all characters
+    SEARCHDIR=`echo $SEARCHDIR | sed -e ' s/ô/Ö/g' -e 's/î/ö/g' -e 's/Ü/å/g' -e 's/è/Å/g' -e 's/Ñ/ä/g'`
+    # Move (rename) the directory
+    mv $CUR_SEARCHDIR $SEARCHDIR
+fi
+
+# Change to the directory to be searched
+cd $SEARCHDIR
+
+# Loop through all directories
+for DIR in `find . -type d`
+do
+    # Check if the special characters exists in the current directory
+    if [[ $DIR == *ô* ]] || [[ $DIR == *î* ]] || [[ $DIR == *Ü* ]] || [[ $DIR == *è* ]] || [[ $DIR == *Ñ* ]]; then
+        # Create temporary variable for the current directory
+        CUR=$DIR
+        # Search and replace all characters
+        NEW=`echo $DIR | sed -e ' s/ô/Ö/g' -e 's/î/ö/g' -e 's/Ü/å/g' -e 's/è/Å/g' -e 's/Ñ/ä/g'`
+        # Move (rename) the directory
+        mv $CUR $NEW
+    fi
+done
---
+++ scripts/timelapse.sh
@@ -0,0 +1,6 @@
+while true; do
+    scrot
+    sleep 1.5s
+end;
+
+# ffmpeg för timelapse
